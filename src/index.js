5786fcb7436ea8235650830e0fcc01542269f0e4
deewan3696 <mail_d1@yahoo.com>
2022-12-04 23:48:29 +0100

    invest and stock

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./App.css"
 // import Register from "./components/Register/Register"
-import VerifyEmailOtp from "./components/Otp/VerifyEmailOtp"
-import VerifyPhoneOtp from "./components/Otp/VerifyPhoneOtp"
-import Dashboard from "./components/Dashboard/Dashboard"
+//import VerifyEmailOtp from "./components/Otp/VerifyEmailOtp"
+//import VerifyPhoneOtp from "./components/Otp/VerifyPhoneOtp"
+//import Dashboard from "./components/Dashboard/Dashboard"
+//import Invest from "./components/Invest/Invest"
+import Stocks from "./components/Stocks/Stocks"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-   <Dashboard />
+   <Stocks />
   );
 
 
