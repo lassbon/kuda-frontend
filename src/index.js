6564f5b71f31f230d118283656f3553fb4eb89d6
rosh <roshbon@gmail.com>
2022-12-05 11:40:10 +0100

    Update index.js
    minor fixes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./App.css"
-// import Register from "./components/Register/Register"
-//import VerifyEmailOtp from "./components/Otp/VerifyEmailOtp"
-//import VerifyPhoneOtp from "./components/Otp/VerifyPhoneOtp"
-//import Dashboard from "./components/Dashboard/Dashboard"
-import AccountDashboard from "./components/Account/AccountDashboard"
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
-   <AccountDashboard />
-  );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
